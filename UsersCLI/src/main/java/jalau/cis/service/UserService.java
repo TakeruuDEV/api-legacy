84774117203ba8c343f8b181d341ed39922e849e
Fernando Marques dos Santos <89764537+TakeruuDEV@users.noreply.github.com>
2024-04-02 23:10:21 -0300

    Create UserService.java

---
+++ UsersCLI/src/main/java/jalau/cis/service/UserService.java
@@ -0,0 +1,45 @@
+package jalau.cis.service;
+
+import jalau.cis.models.User;
+import jalau.cis.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteUser(String id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
